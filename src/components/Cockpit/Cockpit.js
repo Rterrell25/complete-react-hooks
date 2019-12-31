9627acc1b622c982ed068720d358bcfe82f3fbc5
Rob Terrell <Rterrell25@gmail.com>
2019-12-31 11:06:15 -0500

    added meaningful components and messed around with lifecycle hooks to see what was occurring under the hood

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Cockpit.module.css";
 import classes from "./Cockpit.module.css";
 
 export default function Cockpit(props) {
+  useEffect(() => {
+    console.log("[cockpit.js] useEffect");
+    setTimeout(() => {
+      alert("save data to cloud");
+    }, 1000);
+    return () => {
+      console.log("[Cockpit.js] cleanup work");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
+  });
   const assignedClasses = [];
   let btnClass = "";
   if (props.showPersons) {
@@ -16,7 +32,7 @@ export default function Cockpit(props) {
   }
   return (
     <div className={styles.Cockpit}>
-      <h1>Hi, I'm a react app </h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons
